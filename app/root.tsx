d6772c173805357dd48d9ae255c61cbd125998ba
Callie Riggins <calinoracation@gmail.com>
2022-06-25 18:22:09 -0700

    Customization, first pass

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "🍌 Cabana",
   viewport: "width=device-width,initial-scale=1",
 });
 
@@ -21,7 +21,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body style={{ margin: 0 }}>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,21 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ position: 'absolute', inset: '0', display: 'flex', alignItems: 'center', flexDirection: 'column', paddingTop: '15vh' }}>
+      <img 
+        alt="Banana Cabana art, A Banana in a groovy cabana on the beach with another banana watching the sunset." 
+        style={{ aspectRatio: '1', inset: '0', objectFit: 'cover', position: 'fixed', height: '100%', width: '100%' }} 
+        src="/cabana.jpg" 
+      />
+      <h1 style={{ 
+        color: 'hsl(302deg 100% 50%)', 
+        fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"', 
+        fontSize: '14vw', 
+        margin: '0',
+        padding: '0',
+        textShadow: '2px 0 0 #f4fd56, -2px 0 0 #f4fd56, 0 2px 0 #f4fd56, 0 -2px 0 #f4fd56, 1px 1px #f4fd56, -1px -1px 0 #f4fd56, 1px -1px 0 #f4fd56, -1px 1px 0 #f4fd56',
+        zIndex: 1 
+        }
+      }>Coming Soon</h1>
     </div>
   );
 }
